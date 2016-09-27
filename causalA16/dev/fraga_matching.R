a46fd0472d392cd2cc397e9df464f9346bd5d408
Hershberger <hershberger.107@osu.edu>
2016-09-27 15:01:56 -0400

    full update

---
+++ causalA16/dev/fraga_matching.R
@@ -0,0 +1,7 @@
+library(causalA16)
+library(Matching)
+
+Fraga_district_stats <- load("inst/extdata/Fraga2016_DistrictStats.Rdata")
+Fraga_district_treatment <-
+  load("inst/extdata/Fraga2016_DistrictTreatmentResults.Rdata")
+
