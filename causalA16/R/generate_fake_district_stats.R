0f211f1156ce566022bd48d6c0b7744a90c2af0d
Ethan Hershberger <hershberger.107@osu.edu>
2016-12-10 16:45:24 -0500

    Add files via upload

--- causalA16/R/generate_fake_district_stats.R
+++ causalA16/R/generate_fake_district_stats.R
@@ -1,5 +1,6 @@
 district_turnout_sample_1_year <- function(state, pre_district, post_district,
-  n_registrants, turnout_probability,
+  n_registrants, turnout_probability, group, coethnic_incumbent, coethnic_candidate,
+  coethnic_district, perc_fem, perc_55plus, perc_under30, perc_30to55,
   seed = sample.int(.Machine$integer.max, 1))
 {
   library(data.table)
@@ -8,7 +9,15 @@ district_turnout_sample_1_year <- function(state, pre_district, post_district,
      sprintf("%06.f",1:n_registrants))),
    pre_district = pre_district,
    post_district = post_district,
-   state = state)
+   state = state,
+   group = group,
+   coethnic_incumbent = coethnic_incumbent,
+   coethnic_candidate = coethnic_candidate,
+   coethnic_district = coethnic_district,
+   perc_fem = perc_fem,
+   perc_55plus = perc_55plus,
+   perc_under30 = perc_under30,
+   perc_30to55 = perc_30to55)
  turnout_count <- floor(n_registrants * turnout_probability)
  DT[, turnout_1 := 0]
  DT[sample(nrow(DT), turnout_count), turnout_1 := 1]
@@ -78,7 +87,8 @@ district_turnout_next_election <- function(turnout_data, turnout_probability,
 # for getting all elections data for a single subdistrict
 subdistrict_turnout_full <- function(state, pre_district, post_district,
   n_registrants, turnout_probability_vector, irregular_voter_percent,
-  n_elections,
+  n_elections, coethnic_incumbent, coethnic_candidate, coethnic_district,
+  group, perc_fem, perc_55plus, perc_under30, perc_30to55,
   seed = sample.int(.Machine$integer.max, 1)) {
   counter <- 1
   set.seed(seed)
@@ -89,14 +99,20 @@ subdistrict_turnout_full <- function(state, pre_district, post_district,
     cat("*** ", state, pre_district, "to", state, post_district, "\n")
     turnout_data <- district_turnout_sample_1_year(state = state,
       pre_district = pre_district, post_district = post_district,
-      n_registrants = n_registrants,
+      n_registrants = n_registrants, group = group,
+      coethnic_candidate = coethnic_candidate, coethnic_district = coethnic_district,
+      coethnic_incumbent = coethnic_incumbent,
+      perc_fem = perc_fem,
+      perc_55plus = perc_55plus,
+      perc_under30 = perc_under30,
+      perc_30to55 = perc_30to55,
       turnout_probability = turnout_probability_vector[1])
     counter <- counter + 1
   }
     while (counter >= 2 & counter <= n_elections) {
      turnout_data <- district_turnout_next_election(turnout_data = turnout_data,
         turnout_probability = turnout_probability_vector[counter],
-        irregular_voter_percent = irregular_voter_percent)
+        irregular_voter_percent = irregular_voter_percent,)
      counter <- counter + 1
   }
   out <- turnout_data
@@ -115,6 +131,14 @@ turnout_by_subdistrict <- function(district_year_data, irregular_voter_percent) 
       turnout_probability_vector = c(dist_df[1, 7], dist_df[1, 8],
         dist_df[1, 9], dist_df[1, 10]),
       irregular_voter_percent = irregular_voter_percent,
+      group = dist_df[1, 4],
+      coethnic_incumbent = dist_df[1, 31],
+      coethnic_candidate = dist_df[1, 32],
+      coethnic_district = dist_df[1, 33],
+      perc_fem = dist_df[1, 11],
+      perc_55plus = dist_df[1, 14],
+      perc_under30 = dist_df[1, 12],
+      perc_30to55 = dist_df[1, 13],
       n_elections = 4)
     counter <- counter +1
   }
@@ -126,6 +150,14 @@ turnout_by_subdistrict <- function(district_year_data, irregular_voter_percent) 
       turnout_probability_vector = c(dist_df[counter, 7], dist_df[counter, 8],
         dist_df[counter, 9], dist_df[counter, 10]),
       irregular_voter_percent = irregular_voter_percent,
+      group = dist_df[counter, 4],
+      coethnic_incumbent = dist_df[counter, 31],
+      coethnic_candidate = dist_df[counter, 32],
+      coethnic_district = dist_df[counter, 33],
+      perc_fem = dist_df[counter, 11],
+      perc_55plus = dist_df[counter, 14],
+      perc_under30 = dist_df[counter, 12],
+      perc_30to55 = dist_df[counter, 13],
       n_elections = 4)
     dist_DT <- rbind(dist_DT, temp_DT)
     counter <- counter + 1
@@ -133,5 +165,26 @@ turnout_by_subdistrict <- function(district_year_data, irregular_voter_percent) 
   return(dist_DT)
 }
 
-# test <- turnout_by_subdistrict(district_year_wide[State == "AL" & PreDist == 1],
-#   irregular_voter_percent = 0.01)
+make_ip_weights <- function(tr, vars, dat, stabilized = FALSE)
+{
+  if(stabilized == FALSE)
+  {
+    fmla <- as.formula(paste(tr, " ~ ", paste(vars, collapse= "+")))
+    mod <- glm(formula = fmla, data = dat, family = "binomial")
+    dat$prop_score <- predict(mod, type = "response")
+    weights <- ifelse(dat[, c(tr)] == 1, 1/dat$prop_score,
+      1/(1 - dat$prop_score))
+    return(weights)
+  }
+  if(stabilized == TRUE)
+  {
+    prob1 <- (sum(dat[, c(treatment)] == 1)) / (nrow(dat))
+    prob0 <- (sum(dat[, c(treatment)] == 0)) / (nrow(dat))
+    fmla <- as.formula(paste(tr, " ~ ", paste(vars, collapse= "+")))
+    mod <- glm(formula = fmla, data = dat, family = "binomial")
+    dat$prop_score <- predict(mod, type = "response")
+    weights <- ifelse(dat[, c(tr)] == 1, prob1/dat$prop_score,
+      prob0/(1 - dat$prop_score))
+    weights
+  }
+}
